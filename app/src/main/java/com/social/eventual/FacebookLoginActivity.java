e32f87610a952fe3c12e87c34f549408ed74b7f0
naraghisetareh <naraghi.setareh@gmail.com>
2016-05-13 05:49:08 -0700

    onResume and isFacebookLoggedIn functions have been added.... [KingStar]

--- app/src/main/java/com/social/eventual/FacebookLoginActivity.java
+++ app/src/main/java/com/social/eventual/FacebookLoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -126,6 +128,21 @@ public class FacebookLoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isFacebookLoggedIn())
+        {
+            Log.d("Login Status ::::::"," Already Logged In");
+            Intent intent = new Intent(getApplicationContext(),QuizActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean isFacebookLoggedIn(){
+        return AccessToken.getCurrentAccessToken() != null;
     }
 }
 
